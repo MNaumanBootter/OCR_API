a80a8e7412c45a31a34db8357f07c39e034a4991
Muhammad Umer Javed <30436897+muhammadumerjaved44@users.noreply.github.com>
2022-08-21 16:20:03 +0500

    Update database.py

--- database.py
+++ database.py
@@ -22,8 +22,8 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
 def get_db():
-    db = SessionLocal()
     try:
+        db = SessionLocal()
         yield db
     except:
         db.close()
